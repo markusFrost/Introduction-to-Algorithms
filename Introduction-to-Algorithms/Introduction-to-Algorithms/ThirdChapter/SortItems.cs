a30c4f246d799483974144819da4baa50ceb0eaf
markusFrost <andreywystawkin@yandex.ru>
2015-12-06 16:11:29 +0300

    fix bag InsertionSort

--- Introduction-to-Algorithms/Introduction-to-Algorithms/ThirdChapter/SortItems.cs
+++ Introduction-to-Algorithms/Introduction-to-Algorithms/ThirdChapter/SortItems.cs
@@ -74,12 +74,12 @@ namespace Introduction_to_Algorithms.ThirdChapter
             int key = 0;
             int j = 0;
 
-            for (int i = 0; i < n; i++)
+            for (int i = 1; i < n; i++)
             {
                 key = a[i];
                 j = i - 1;
 
-                while (j > 0 && a[j] > key)
+                while (j >= 0 && a[j] > key)
                 {
                     a[j + 1] = a[j];
                     j--;
